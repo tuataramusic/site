4bbc61b6cfd5fcac17c1d8c5275787634d7fc219
Craftsman <658232@gmai.com>
2011-05-13 18:17:05 +0400

    IE6 заглушка (fix)

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <?php
 $user_agent = $_SERVER['HTTP_USER_AGENT'];
 if (stripos($user_agent, 'MSIE 6.0') !== false && stripos($user_agent, 'MSIE 8.0') === false && stripos($user_agent, 'MSIE 7.0') === false) {
-   if (!isset($HTTP_COOKIE_VARS["ie"])) {setcookie("ie", "yes", time()+60*60*24*360);header ("Location: public_html/static/ie6/ie6.html");}
+   if (!isset($HTTP_COOKIE_VARS["ie"])) {setcookie("ie", "yes", time()+60*60*24*360);header ("Location: static/ie6/ie6.html");}
 }
-
-<?php
 /*
 |---------------------------------------------------------------
 | PHP ERROR REPORTING LEVEL
